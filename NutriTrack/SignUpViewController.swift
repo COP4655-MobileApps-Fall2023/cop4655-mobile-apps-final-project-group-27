834e802e353097a66937da88c5337270766b7c6c
Dante Ricketts <dante.ricketts16@gmail.com>
2023-11-07 15:24:14 -0500

    Add backend to login and sign up buttons

--- NutriTrack/SignUpViewController.swift
+++ NutriTrack/SignUpViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import ParseSwift
 
 class SignUpViewController: UIViewController {
 
     @IBOutlet weak var Email_Field: UITextField!
     @IBOutlet weak var Username_Field: UITextField!
     @IBOutlet weak var Password_Field: UITextField!
-    @IBOutlet weak var ReEnter_Password_Field: UITextField!
+    
     @IBAction func SignUp_Button(_ sender: Any) {
+        guard let username = Username_Field.text,
+              let email = Email_Field.text,
+              let password = Password_Field.text,
+              !username.isEmpty,
+              !email.isEmpty,
+              !password.isEmpty else{
+            showMissingFieldsAlert()
+            return
+        }
+        var newUser = User()
+        newUser.username = username
+        newUser.email = email
+        newUser.password = password
+        newUser.signup{ [weak self] result in
+            switch result{
+            case .success(let user):
+                print("✅ Successfully signed up user \(user)")
+                NotificationCenter.default.post(name: Notification.Name("login"), object: nil)
+            case .failure(let error):
+                
+                self?.showAlert(description: error.localizedDescription)
+                
+            }
+            
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +48,17 @@ class SignUpViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+    private func showAlert(description: String?) {
+          let alertController = UIAlertController(title: "Unable to Sign Up", message: description ?? "Unknown error", preferredStyle: .alert)
+          let action = UIAlertAction(title: "OK", style: .default)
+          alertController.addAction(action)
+          present(alertController, animated: true)
+      }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+      private func showMissingFieldsAlert() {
+          let alertController = UIAlertController(title: "Opps...", message: "We need all fields filled out in order to sign you up.", preferredStyle: .alert)
+          let action = UIAlertAction(title: "OK", style: .default)
+          alertController.addAction(action)
+          present(alertController, animated: true)
+      }
 }
