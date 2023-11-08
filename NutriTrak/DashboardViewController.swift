f3ed1d5ba50a9dc57ad5de0077a4b9e212641175
JovanF <fullerj2020@fau.edu>
2023-11-07 22:25:15 -0500

    Profile and Dashboard Screens

---
+++ NutriTrak/DashboardViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DashboardController.swift
+//  NutriTrak
+//
+//  Created by Jovan Fuller on 11/7/23.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+
+    @IBOutlet weak var calorieCounter: UITextField!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var addFoodButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NutriTrak/ProfileViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AccountViewController.swift
+//  NutriTrak
+//
+//  Created by Jovan Fuller on 11/7/23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var navBarTitle: UINavigationItem!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileName: UITextField!
+    
+    @IBOutlet weak var editProfileButton: UIButton!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
