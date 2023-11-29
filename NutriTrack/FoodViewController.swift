b783d0a4fba0f74f43f3e11db5cc694402430de3
christian <74482455+callbright@users.noreply.github.com>
2023-11-28 19:31:16 -0500

    food screen

--- NutriTrack/FoodViewController.swift
+++ NutriTrack/FoodViewController.swift
@@ -1,74 +1,91 @@
-//
-//  FoodViewController.swift
-//  NutriTrack
-//
-//  Created by Dante Ricketts on 11/23/23.
-//
-
 import UIKit
 
-class FoodViewController: UIViewController, UITableViewDataSource {
+class FoodViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
+
+    // Your existing properties
+    var foods: [Food] = []
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        searchBar.delegate = self
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return foods.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Your existing implementation
         let cell = tableView.dequeueReusableCell(withIdentifier: "FoodCell", for: indexPath) as! FoodCell
-        let food = foods[indexPath.row]
-        cell.configure(with: food)
-        return cell
-        
+                let food = foods[indexPath.row]
+                cell.configure(with: food)
+                return cell
     }
-    
-    
-    
-    var foods: [Food] = []
-    
 
-    @IBOutlet weak var tableView: UITableView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchText = searchBar.text, !searchText.isEmpty else {
+            return
+        }
+        fetchNutritionData(for: searchText)
+    }
 
-        tableView.dataSource = self
-        let url = URL(string: "https://api.api-ninjas.com/v1/nutrition?query=brisket")!
-        let request = URLRequest(url:url)
-        let task = URLSession.shared.dataTask(with: request){ [weak self] data, response, error in
-            
+    private func fetchNutritionData(for query: String) {
+        guard let encodedQuery = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let url = URL(string: "https://api.api-ninjas.com/v1/nutrition?query=\(encodedQuery)") else {
+                  print("Invalid URL or query")
+                  return
+        }
+        print("Making API call to URL: \(url)")
+        var request = URLRequest(url: url)
+        request.setValue("XJjUAoJeFY5uofyt/xl8bg==FjwSc1CVBwCE2rUT", forHTTPHeaderField: "X-Api-Key")
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
             if let error = error {
-                print("❌ Network error: \(error.localizedDescription)")
-            }
-            guard let data = data else{
-                print("❌ Data is nil")
+                print("API call failed with error: \(error.localizedDescription)")
                 return
             }
+            guard let data = data else {
+                print("Received no data from API")
+                return
+            }
+            print("Received data: \(String(describing: String(data: data, encoding: .utf8)))")
             let decoder = JSONDecoder()
             do {
-                let response = try decoder.decode(FoodsResponse.self, from: data)
-                let foods = response.results
-                DispatchQueue.main.async {
-                    self?.foods = foods
-                    self?.tableView.reloadData()
+                        let foods = try JSONDecoder().decode([Food].self, from: data)
+                        
+                        // Construct a message with the results
+                        var resultsMessage = "Search Results:\n"
+                        for food in foods {
+                            resultsMessage += "\(food.name): \(food.calories) calories\n"
+                        }
+
+                        DispatchQueue.main.async {
+                            self?.foods = foods
+                            self?.tableView.reloadData()
+
+                            // Show the results in an alert
+                            self?.showAlert(message: resultsMessage)
+                        }
+                    } catch {
+                        DispatchQueue.main.async {
+                            self?.showAlert(message: "Error parsing JSON: \(error.localizedDescription)")
+                        }
+                    }
                 }
-                
-            }catch{
-                print("❌ Error parsing JSON: \(error.localizedDescription)")
-                
+                task.resume()
             }
-        }
-        task.resume()
-        print(foods)
-    }
+
+            private func showAlert(message: String) {
+                let alert = UIAlertController(title: "Search Results", message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                DispatchQueue.main.async {
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Rest of your code
 }
-
